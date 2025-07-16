c595ebd90ae2dab7c8859fe99fa8599e28d2b6b4
Jasa Fridau <fridaujasa@gmail.com>
2025-07-17 00:23:00 +0200

    shop archive improvements

--- core.php
+++ core.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-if (!defined('ABSPATH')) {
+if (!defined("ABSPATH")) {
     exit();
 }
 
 function getComponent($name, $props = [])
 {
     ob_start();
-    get_template_part('/components/' . $name, null, $props);
+    get_template_part("/components/" . $name, null, $props);
     $output = ob_get_clean();
     return $output;
 }
@@ -19,7 +19,9 @@ function setupComponent($args, $requiredProps = [])
     if (!empty($requiredProps)) {
         foreach ($requiredProps as $requiredProp) {
             if (!array_key_exists($requiredProp, $args)) {
-                throw new Exception('<strong>Error</strong>: missing Prop ' . $requiredProp);
+                throw new Exception(
+                    "<strong>Error</strong>: missing Prop " . $requiredProp,
+                );
                 die();
             }
         }
@@ -29,5 +31,5 @@ function setupComponent($args, $requiredProps = [])
 
 function useImage($imageName)
 {
-    return esc_url(THEME_URI . 'dist/images/' . $imageName);
+    return esc_url(THEME_URI . "dist/images/" . $imageName);
 }
--- functions/shop/archive.php
+++ functions/shop/archive.php
@@ -18,6 +18,51 @@ function kumo_shop_wrapper()
 
 add_filter("woocommerce_show_page_title", "__return_false");
 
+add_action(
+    "woocommerce_before_main_content",
+    "custom_woocommerce_archive_header_image",
+    5,
+);
+
+function custom_woocommerce_archive_header_image()
+{
+    if (!is_shop() && !is_product_category()) {
+        return;
+    }
+
+    $background_image = "";
+
+    if (is_product_category()) {
+        $term = get_queried_object();
+        $image_id = get_field(
+            "category_header_image",
+            "product_cat_" . $term->term_id,
+        )["ID"];
+
+        if ($image_id) {
+            $background_image = wp_get_attachment_image_url($image_id, "full");
+        }
+
+        $title = $term->name;
+    } else {
+        // Shop page
+        $image_id = get_field("shop_header_image", "option")["ID"];
+
+        if ($image_id) {
+            $background_image = wp_get_attachment_image_url($image_id, "full");
+        }
+
+        $title = "MALL";
+    }
+
+    echo '<div class="theme-shop-header" style="background-image: url(' .
+        esc_url($background_image) .
+        ');">';
+    echo '<div class="theme-shop-header-overlay">';
+    echo '<h1 class="theme-shop-header__title">' . esc_html($title) . "</h1>";
+    echo "</div></div>";
+}
+
 add_action("woocommerce_after_main_content", "kumo_shop_close", 99);
 
 function kumo_shop_close()
